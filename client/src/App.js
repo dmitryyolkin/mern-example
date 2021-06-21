a07b56b37d8fa1335fcd1c156b5491545ce38c3c
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2021-06-21 09:13:48 +0400

    Add NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { useRoutes } from "./routes";
 import { useAuth } from "./hooks/auth.hook";
 import { AuthContext } from "./context/auth.context";
+import { NavBar } from "./components/NavBar";
 import "materialize-css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       value={{ login, logout, userId, token, isAuthenticated, tokenVerified }}
     >
       <Router>
+        {isAuthenticated && <NavBar />}
         <div className="container">{routes}</div>;
       </Router>
     </AuthContext.Provider>
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
+
+export const NavBar = () => {
+  const auth = useContext(AuthContext);
+  const history = useHistory();
+  const logoutHandler = (event) => {
+    event.preventDefault(); // prevent a default behaviour
+    auth.logout();
+    history.push("/");
+  };
+
+  return (
+    <nav>
+      <div className="nav-wrapper blue darken-1 navbar-links">
+        <span className="brand-logo">Links Dictionary</span>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <NavLink to="/create">Create</NavLink>
+          </li>
+          <li>
+            <NavLink to="/links">Links</NavLink>
+          </li>
+          <li>
+            <a href="/" onClick={logoutHandler}>
+              Logout
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
